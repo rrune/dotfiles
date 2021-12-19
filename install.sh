7ed918a25b27b2e664e8dac807a742ed749dd224
rune <rune@ruune.de>
2021-12-19 20:48:45 +0100

    changes

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 read -p "-> This will replace some stuff. You sure? [Y/n] " -n 1 -r
 echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     echo "-> Canceling"
     exit 1
 fi
@@ -12,8 +11,7 @@ echo "-> Installing Dotfiles"
 
 read -p "-> Generate SSH keys? [Y/n] " -n 1 -r
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "-> Generating SSH key"
     ssh-keygen -t rsa -b 4096
 fi
@@ -29,43 +27,53 @@ ln -sf ${SCRIPT_DIR}/config/starship.toml ~/.config/starship.toml
 
 read -p "-> Install Starship prompt? [Y/n] " -n 1 -r
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
     sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+    echo "-> Installing Fira Code font"
+    mkdir ~/.local/share/fonts >/dev/null 2>&1
+    cp ./firacode.ttf ~/.local/share/fonts >/dev/null 2>&1
+    fc-cache -f -v >/dev/null 2>&1
 fi
 
 read -p "-> Install scripts (External sources)? [Y/n] " -n 1 -r
 echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     echo "-> Installed configs without scripts, you will get errors"
     exit 1
 fi
 
-echo "-> Installing Fira Code font"
-mkdir ~/.local/share/fonts > /dev/null 2>&1
-cp ./firacode.ttf ~/.local/share/fonts > /dev/null 2>&1
-fc-cache -f -v > /dev/null 2>&1
-
 echo "-> Creating scripts directory"
-mkdir ~/stuff/scripts > /dev/null 2>&1
+mkdir ~/stuff/scripts >/dev/null 2>&1
 
 echo "-> Installing goto"
-wget -O ~/stuff/scripts/goto.sh https://raw.githubusercontent.com/iridakos/goto/master/goto.sh > /dev/null 2>&1
+curl -o ~/stuff/scripts/goto.sh https://raw.githubusercontent.com/iridakos/goto/master/goto.sh >/dev/null 2>&1
 
 echo "-> Installing up"
-wget -O ~/stuff/scripts/up.sh https://raw.githubusercontent.com/shannonmoeller/up/master/up.sh > /dev/null 2>&1
-
+curl -o ~/stuff/scripts/up.sh https://raw.githubusercontent.com/shannonmoeller/up/master/up.sh >/dev/null 2>&1
 
 echo "-> Creating directory for PATH"
-mkdir ~/stuff/scripts/bin > /dev/null 2>&1
+mkdir ~/stuff/scripts/bin >/dev/null 2>&1
 
-echo "-> Installing ext"
-wget -O ~/stuff/scripts/bin/ext.sh https://raw.githubusercontent.com/Manas140/sh/main/src/ext > /dev/null 2>&1
-mv ~/stuff/scripts/bin/ext.sh ~/stuff/scripts/bin/ext > /dev/null 2>&1
+read -p "-> Sudo needed for chmod. Install? [Y/n] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sudo -k
+    if sudo true; then
+        echo "-> Installing ext"
+        curl -o ~/stuff/scripts/bin/ext.sh https://raw.githubusercontent.com/Manas140/sh/main/src/ext >/dev/null 2>&1
+        mv ~/stuff/scripts/bin/ext.sh ~/stuff/scripts/bin/ext >/dev/null 2>&1
 
-echo "-> Installing calc"
-wget -O ~/stuff/scripts/bin/calc.sh https://raw.githubusercontent.com/Manas140/sh/main/src/calc > /dev/null 2>&1
-mv ~/stuff/scripts/bin/calc.sh ~/stuff/scripts/bin/calc > /dev/null 2>&1
+        echo "-> Installing calc"
+        curl -o ~/stuff/scripts/bin/calc.sh https://raw.githubusercontent.com/Manas140/sh/main/src/calc >/dev/null 2>&1
+        mv ~/stuff/scripts/bin/calc.sh ~/stuff/scripts/bin/calc >/dev/null 2>&1
 
-echo "-> Everything installed. Restart or type 'source ~/.bashrc'"
+        sudo chmod +x ~/stuff/scripts/bin/ext
+        sudo chmod +x ~/stuff/scripts/bin/calc
+    else
+        echo "-> Wrong password. Skiping"
+    fi
+
+fi
+
+echo "-> Everything installed. Restart terminal or type 'source ~/.bashrc'"
