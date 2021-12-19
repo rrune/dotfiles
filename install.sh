81039deb5ec4d4345b4f9497fc473b9c1846d1ce
rune <rune@ruune.de>
2021-12-19 15:53:12 +0100

    broken

--- install.sh
+++ install.sh
@@ -1,52 +1,81 @@
-read -p "This will replace some stuff. You sure? [Y/n] " -n 1 -r
-#echo    # (optional) move to a new line
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+read -p "-> This will replace some stuff. You sure? [Y/n] " -n 1 -r
+echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-    echo "Canceling"
+    echo "-> Canceling"
     exit 1
 fi
 
-echo "Installing Dotfiles"
+echo "-> Installing Dotfiles"
 
-echo "Generating SSH Key"
-ssh-keygen -t rsa -b 4096
+read -p "-> Generate SSH keys? [Y/n] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "-> Generating SSH key"
+    ssh-keygen -t rsa -b 4096
+fi
 
-echo "Linking bashrc"
-ln -s ./bashrc ~/.bashrc
+echo "-> Linking bashrc"
+ln -sf ${SCRIPT_DIR}/bashrc ~/.bashrc
 
-echo "Linking ~/.config/nvim"
-ln -s ./config/nvim ~/.config/nvim
+echo "-> Linking ~/.config/nvim"
+ln -sf ${SCRIPT_DIR}/config/nvim ~/.config/nvim
 
-echo "Linking starship.toml"
-ln -s ./config/starship.toml ~/.config/starship.toml
+echo "-> Linking starship.toml"
+ln -sf ${SCRIPT_DIR}/config/starship.toml ~/.config/starship.toml
 
-read -p "Install prompt and scripts (External sources)? [Y/n] " -n 1 -r
+read -p "-> Install prompt and scripts (External sources)? [Y/n] " -n 1 -r
+echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-    echo "Installed configs without scripts, you will get errors"
+    echo "-> Installed configs without scripts, you will get errors"
     exit 1
 fi
 
-echo "Installing Starship prompt"
-source "$(curl -fsSL https://starship.rs/install.sh)"
+echo "-> Installing Fira Code font"
+{
+    mkdir ~/.local/share/fonts
+    cp ./firacode.ttf ~/.local/share/fonts
+    fc-cache -f -v
+} &> /dev/null
+wait
 
-echo "Creating scripts directory"
-mkdir ~/stuff/scripts
+echo "-> Installing Starship prompt"
+{source "$(curl -fsSL https://starship.rs/install.sh)"} &> /dev/null
+wait
 
-echo "Installing goto"
-wget -o ~/stuff/scripts/goto.sh https://raw.githubusercontent.com/iridakos/goto/master/goto.sh
+echo "-> Creating scripts directory"
+{mkdir ~/stuff/scripts} &> /dev/null
+wait
 
-echo "Installing up"
-wget -o ~/stuff/scripts/up.sh
+echo "-> Installing goto"
+{wget -O ~/stuff/scripts/goto.sh https://raw.githubusercontent.com/iridakos/goto/master/goto.sh} &> /dev/null
+wait
 
-echo "Creating directory for PATH"
-mkdir ~/stuff/scripts/bin
+echo "-> Installing up"
+{wget -O ~/stuff/scripts/up.sh https://raw.githubusercontent.com/shannonmoeller/up/master/up.sh} &> /dev/null
+wait
 
-echo "Installing ext"
-wget -o ~/stuff/scripts/bin/ext https://raw.githubusercontent.com/Manas140/sh/main/src/External
+echo "-> Creating directory for PATH"
+{mkdir ~/stuff/scripts/bin} &> /dev/null
+wait
 
-echo "Installing calc"
-wget -o ~/stuff/scripts/bin/calc https://raw.githubusercontent.com/Manas140/sh/main/src/calc
+echo "-> Installing ext"
+{
+    wget -O ~/stuff/scripts/bin/ext.sh https://raw.githubusercontent.com/Manas140/sh/main/src/ext
+    mv ~/stuff/scripts/bin/ext.sh ~/stuff/scripts/bin/ext
+}&> /dev/null
+wait
 
-echo "Everything installed"
+echo "-> Installing calc"
+{
+    wget -O ~/stuff/scripts/bin/calc.sh https://raw.githubusercontent.com/Manas140/sh/main/src/calc
+    mv ~/stuff/scripts/bin/calc.sh ~/stuff/scripts/bin/calc
+} &> /dev/null
+wait
+
+echo "-> Everything installed"
 source ~/.bashrc
